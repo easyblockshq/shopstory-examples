c5cd55056a80eac629dfb08b3892906a92c6945f
pawelshopstory <95621462+pawelshopstory@users.noreply.github.com>
2022-12-08 11:49:13 +0100

    Adjust Gatsby integration to monorepo setup

---
+++ examples/gatsby-4/src/shopstory/config.ts
@@ -0,0 +1,16 @@
+import { Config } from "@shopstory/core";
+import { contentfulPlugin } from "@shopstory/core/contentful";
+import { shopstoryBaseConfig } from "../../../../shared/shopstory/config";
+
+export const shopstoryConfig: Config = {
+  ...shopstoryBaseConfig,
+  plugins: [
+    contentfulPlugin({
+      space: process.env.GATSBY_PUBLIC_CONTENTFUL_SPACE!,
+      environment: process.env.GATSBY_PUBLIC_CONTENTFUL_ENVIRONMENT ?? "master",
+      accessToken: process.env.GATSBY_PUBLIC_CONTENTFUL_ACCESS_TOKEN!,
+      previewAccessToken:
+        process.env.GATSBY_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN!,
+    }),
+  ],
+};
