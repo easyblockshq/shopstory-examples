75f74b8392a6f514825c5834e22448d04b72e744
pawelshopstory <95621462+pawelshopstory@users.noreply.github.com>
2023-03-30 15:19:09 +0200

    Fix sanity build

--- shared/components/Toast/ToastPortal.tsx
+++ shared/components/Toast/ToastPortal.tsx
@@ -1,12 +1,14 @@
-import React, { FC } from 'react'
-import * as ReactDOM from 'react-dom'
+import React, { FC, ReactNode } from "react";
+import * as ReactDOM from "react-dom";
 
-export const ToastPortal: FC = ({ children }) => {
-  if (typeof window === 'object') {
-    const toastRoot = document.getElementById('toastContainer') || document.createElement('div')
-    toastRoot.className = 'toastContainer'
+export const ToastPortal: FC<{ children: ReactNode }> = ({ children }) => {
+  if (typeof window === "object") {
+    const toastRoot =
+      document.getElementById("toastContainer") ||
+      document.createElement("div");
+    toastRoot.className = "toastContainer";
 
-    return ReactDOM.createPortal(children, toastRoot)
+    return ReactDOM.createPortal(children, toastRoot);
   }
-  return null
-}
+  return null;
+};
