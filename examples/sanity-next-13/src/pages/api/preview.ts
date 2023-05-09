598e93cbf9ef85f9b984e844a11ad4722983cd90
pawelshopstory <95621462+pawelshopstory@users.noreply.github.com>
2023-05-09 10:20:16 +0200

    fix(sanity-next-13): fix broken preview link

--- examples/sanity-next-13/src/pages/api/preview.ts
+++ examples/sanity-next-13/src/pages/api/preview.ts
@@ -4,9 +4,8 @@ export default function preview(req: NextApiRequest, res: NextApiResponse) {
   const { page } = req.query;
   res.setPreviewData({});
   res.writeHead(307, {
-    Location: process.env.VERCEL
-      ? "https://"
-      : "http://" + req.headers.host + page,
+    Location:
+      (process.env.VERCEL ? "https://" : "http://") + req.headers.host + page,
   });
   res.end();
 }
