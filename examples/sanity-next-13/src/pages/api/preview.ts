4ad340a4c0dc5135155684c742659c615151cadc
pawelshopstory <95621462+pawelshopstory@users.noreply.github.com>
2023-05-09 10:11:09 +0200

    fix(sanity-next-13): add https protocol when enabling preview in Vercel env

--- examples/sanity-next-13/src/pages/api/preview.ts
+++ examples/sanity-next-13/src/pages/api/preview.ts
@@ -3,6 +3,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default function preview(req: NextApiRequest, res: NextApiResponse) {
   const { page } = req.query;
   res.setPreviewData({});
-  res.writeHead(307, { Location: "http://" + req.headers.host + page });
+  res.writeHead(307, {
+    Location: process.env.VERCEL
+      ? "https://"
+      : "http://" + req.headers.host + page,
+  });
   res.end();
 }
